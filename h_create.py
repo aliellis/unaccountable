efa65befb109323f3e5cc586c9ec8ecd92c89d76
Alasdair Ellis <alasdair@skimlinks.com>
2015-04-18 15:09:27 +0100

    Cleaned up endpoint, added more methods to hipchat integration

--- h_create.py
+++ h_create.py
@@ -5,28 +5,55 @@ import yaml
 
 import ipdb
 
+import urllib
+import urlparse
+
 config = open("config.yml", "r")
 config = yaml.load(config)
 
 AUTH_TOKEN = config["auth_token"]
+ENDPOINT = "https://api.hipchat.com/v2/"
 
-url = "https://api.hipchat.com/v2/room?auth_token=" + AUTH_TOKEN
-
-request = urllib2.Request(url)
-response = urllib2.urlopen(request)
 
 def all_users():
-    url = "https://api.hipchat.com/v2/user?auth_token=" + AUTH_TOKEN
+    url = ENDPOINT + "user?auth_token=" + AUTH_TOKEN
     request = urllib2.Request(url)
     response = urllib2.urlopen(request)
     response = json.load(response)
     response = response["items"]
     return [user["name"] for user in response]
 
+
 def all_rooms():
-    url = "https://api.hipchat.com/v2/room?auth_token=" + AUTH_TOKEN
+    url = ENDPOINT + "room?auth_token=" + AUTH_TOKEN + "&private-room=true"
     request = urllib2.Request(url)
     response = urllib2.urlopen(request)
     response = json.load(response)
     response = response["items"]
-    return[room["name"] for room in response]
+    return [room["name"] for room in response]
+
+
+def get_user_info(user):
+    url = ENDPOINT + "user/" + user + "?auth_token=" + AUTH_TOKEN
+    request = urllib2.Request(url)
+    response = urllib2.urlopen(request)
+    return json.load(response)
+
+
+def get_room_members(group):
+    url = ENDPOINT + "room/" + group + "?auth_token=" + AUTH_TOKEN
+    url = url_fix(url)
+    request = urllib2.Request(url)
+    response = urllib2.urlopen(request)
+    response = json.load(response)
+    return [user["name"] for user in response["participants"]]
+
+
+def url_fix(s, charset='utf-8'):
+    """Grabbed from http://stackoverflow.com/questions/120951/how-can-i-normalize-a-url-in-python"""
+    if isinstance(s, unicode):
+        s = s.encode(charset, 'ignore')
+    scheme, netloc, path, qs, anchor = urlparse.urlsplit(s)
+    path = urllib.quote(path, '/%')
+    qs = urllib.quote_plus(qs, ':&=')
+    return urlparse.urlunsplit((scheme, netloc, path, qs, anchor))
