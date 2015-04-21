5bee2ac8ceffe22efcf5d7441f1efb4fc8e8e69c
Alasdair Ellis <alasdair@skimlinks.com>
2015-04-21 16:39:24 +0100

    basic cacheing

---
+++ fn_cache.py
@@ -0,0 +1,24 @@
+import collections
+import functools
+
+
+class Memoized(object):
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+
+    def __call__(self, *args):
+        if not isinstance(args, collections.Hashable):
+            return self.func(*args)
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+
+    def __repr__(self):
+        return self.func.__doc__
+
+    def __get__(self, obj, objtype):
+        return functools.partial(self.__call__, obj)
