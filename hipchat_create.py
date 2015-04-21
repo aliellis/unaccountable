2aa38656c6d42386724e08a9bb23358cab29e6bc
Alasdair Ellis <alasdair@skimlinks.com>
2015-04-21 16:42:32 +0100

    Begun work on implementing a complete list of all user data from hipchat

--- hipchat_create.py
+++ hipchat_create.py
@@ -2,6 +2,8 @@ import pprint
 import json
 import urllib2
 import yaml
+import time
+from fn_cache import Memoized
 
 import ipdb
 
@@ -12,10 +14,13 @@ config = open("config.yml", "r")
 config = yaml.load(config)
 
 AUTH_TOKEN = config["hipchat_auth_token"]
+USER_PW = config["h_user_pw"]
 ENDPOINT = "https://api.hipchat.com/v2/"
 
 
 def all_users():
+    # TODO: create a function that you pass 'name', 'email', 'rights', etc 
+    # to make this more modular
     url = ENDPOINT + "user?auth_token=" + AUTH_TOKEN
     request = urllib2.Request(url)
     response = urllib2.urlopen(request)
@@ -61,3 +66,58 @@ def url_fix(s, charset='utf-8'):
     path = urllib.quote(path, '/%')
     qs = urllib.quote_plus(qs, ':&=')
     return urlparse.urlunsplit((scheme, netloc, path, qs, anchor))
+
+
+def detect_duplicate():
+    return
+
+@Memoized
+def all_user_info():
+    # full json response for all users
+    url = ENDPOINT + "user?auth_token=" + AUTH_TOKEN
+    request = urllib2.Request(url)
+    response = urllib2.urlopen(request)
+    response = json.load(response)
+    return response["items"]
+
+
+def generate_unique_identifiers():
+    res = {
+           "mention_name": [],
+           "email": [],
+           "name": []
+          }
+
+    print "Generating..."
+    all_info = all_user_info()
+    for user in all_info:
+        # import ipdb
+        # ipdb.set_trace()
+        print "adding uniques from " + user["name"]
+        res["email"].append(get_user_info(str(user["id"]))["email"])
+        res["mention_name"].append(user["mention_name"])
+        res["name"].append(user["name"])
+        print "uniques added, sleeping for 20 seconds to avoid rate limiting"
+        # time.sleep(20)
+    return res
+
+# @Memoized
+# def fibonacci(n):
+#     "Return the nth fibonacci number."
+#     if n in (0, 1):
+#         return n
+#     return fibonacci(n-1) + fibonacci(n-2)
+# 
+# print fibonacci(12)
+
+
+    # json = {
+    #         "email": 'test',
+    #         "name:" 'test',
+    #         "mention_name": 'test',
+    #         "password": 'test'
+    #        }
+    # return json
+
+
+print generate_unique_identifiers()
