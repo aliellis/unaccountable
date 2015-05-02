2450453aba58edc8b29b2c48dc1bac99528111ca
Alasdair Ellis <alasdair@skimlinks.com>
2015-05-02 14:31:18 +0100

    Finally moved out the print_col_to_string function to table_helpers/whatever-i-want-to-call-it

--- cmd_display.py
+++ cmd_display.py
@@ -11,10 +11,16 @@ def validate_columns(t_data):
     return t_data
 
 
-def generate_table(dict):
+def generate_table(t_dict):
     table = PrettyTable()
-    for key in dict:
-        col = dict[key]
-        table.add_column(key, dict[key])
+    for key in t_dict:
+        col = t_dict[key]
+        table.add_column(key, t_dict[key])
     table.align = "l"
     return table
+
+
+def table_contents_to_s(t_dict):
+    for key in t_dict:
+        t_dict[key] = [str(t_dict[key])]
+    return t_dict
