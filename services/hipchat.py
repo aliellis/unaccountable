87f9e51cd47c3cb02f768ddeccb108661d530852
Alasdair Ellis <alasdair@skimlinks.com>
2015-05-05 12:26:07 +0100

    HipChat create now running in a class, limited error handling for checking for duplicate email/mentions

--- services/hipchat.py
+++ services/hipchat.py
@@ -4,6 +4,8 @@ import urllib
 import urlparse
 import requests
 
+import pprint
+import re
 
 """ NOTE: Due to rate limiting, and the way HipChat handles user info requests,
     it is currently impossible to fetch ALL user data within a reasonable amount
@@ -19,6 +21,7 @@ class Hipchat():
         self.endpoint = "https://api.hipchat.com/v2/"
         self.auth_token = config["hipchat_auth_token"]
         self.user_pass = config["hipchat_user_pass"]
+        self.domain = config["domain"]
 
     # Can probably remove this when we port over to requests
     # Necessary to handle white space in get_user_info requests
@@ -39,6 +42,13 @@ class Hipchat():
         response = response["items"]
         return [user["name"] for user in response]
 
+    def all_users_raw(self):
+        url = "{}user?auth_token={}".format(self.endpoint, self.auth_token)
+        request = urllib2.Request(url)
+        response = urllib2.urlopen(request)
+        response = json.load(response)
+        return response
+
     def get_user(self, user):
         url = self.endpoint + "user/" + user + "?auth_token=" + self.auth_token
         request = urllib2.Request(url)
@@ -55,21 +65,102 @@ class Hipchat():
         except:
             return False
 
-    def create_user(user_email, user_name, user_mention, pw=None):
-        if pw is None:
-            pw = USER_PW
-        host = "api.hipchat.com"
-        url = "https://{0}/v2/user".format(host)
-        headers = {'Content-type': 'application/json'}
-        headers['Authorization'] = "Bearer " + AUTH_TOKEN
-        params = {"auth_token": AUTH_TOKEN}
-        payload = {
-            'email': user_email,
-            'name': user_name,
-            'mention_name': user_mention,
-            'password': pw
-        }
-        requests.post(url, params=params, data=json.dumps(payload), headers=headers)
+    def generate_user_template(self):
+        print "Please enter the user's first and last name: "
+        user = raw_input("> ")
+        template = self.generate_user_template_from_name(user)
+        print ""
+        pprint.pprint(template)
+        print ""
+        print "Is this ok? y/n"
+        yn = raw_input("> ")
+        if yn.lower() == "y":
+            return template
+        elif yn.lower() == "n":
+            print "generating custom template"
+            custom_template = self.generate_user_template_interactive(user)
+            while custom_template is None:
+                custom_template = self.generate_user_template_interactive(user)
+            else:
+                return custom_template
+
+    def generate_user_template_interactive(self, user):
+        print "Please enter the user's default password"
+        pw = raw_input("> ")
+        print "Please enter the user's email address"
+        email = raw_input("> ")
+        print "Please enter the user's mention name"
+        mention = raw_input("> ")
+
+        template = {
+                    "email": email,
+                    "name": user,
+                    "mention_name": mention,
+                    "password": pw,
+                   }
+        print ""
+        pprint.pprint(template)
+        print ""
+        print "Is this ok? y/n"
+        yn = raw_input("> ")
+        if yn.lower() == "y":
+            return template
+        elif yn.lower() == "n":
+            return
+
+    def generate_user_template_from_name(self, user):
+        template = {
+                    "email": user.split()[0].lower() + "@" + self.domain,
+                    "name": user,
+                    "mention_name": user.split()[0] + user.split()[1][0],
+                    "password": self.user_pass
+                   }
+        return template
+
+    def is_not_duplicate(self, user, field):
+        desired = user[field]
+        patt = "^({}.*)".format(desired[0].lower())
+        regex = re.compile(patt)
+        invalid = self.get_similar_acounts(regex)
+        if invalid:
+            similar = []
+            for user_id in invalid:
+                similar.append(self.get_user(user_id)[field])
+            if desired in similar:
+                print "desired field, {} is already in use by another HipChat user, exiting...".format(desired)
+                return False
+            else:
+                return True
+
+    def create_user(self, user):
+        print "ensuring email address and mention name is not already taken"
+
+        if self.is_not_duplicate(user, "email") and self.is_not_duplicate(user, "mention_name"):
+            print "no duplicates exist, creating account"
+
+            url = "{}user".format(self.endpoint)
+            headers = {"Content-type": "application/json"}
+            headers["Authorization"] = "Bearer " + self.auth_token
+            params = {"auth_token": self.auth_token}
+
+            requests.post(url,
+                          params=params,
+                          data=json.dumps(user),
+                          headers=headers)
+            print "user created"
+
+        else:
+            print "duplicates exist, exiting..."
+
+    def get_similar_acounts(self, search_p):
+        all_users = self.all_users_raw()
+        all_names = [account["name"].lower() for account in all_users["items"]]
+        possible_matches = []
+        for name in all_users["items"]:
+            if name["name"]:
+                if search_p.match(name["name"].lower()):
+                    possible_matches.append(str(name["id"]))
+        return possible_matches
 
     def get_all_rooms():
         url = self.endpoint + "room?auth_token=" + self.auth_token + "&private-room=true"
